fb63966258677df6353917fc411528d2fafa7dff
Bryan Pritchett <bryan@coderbox.co>
2023-02-26 14:10:15 -0600

    Update bryan_install.sh

--- bryan_install.sh
+++ bryan_install.sh
@@ -62,7 +62,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 #    	Z Shell: ~/.zshrc or .zprofile
 # 
 	dotnet_sdk=https://download.visualstudio.microsoft.com/download/pr/ec76d8ee-8776-42ce-b158-f723a221fc56/0baa1089edf0e0674d719f6a5d847b75/dotnet-sdk-7.0.201-linux-x64.tar.gz
-	wget $dotnet_sdk -0 dotnet-sdk.tar.gz
+	wget $dotnet_sdk -O dotnet-sdk.tar.gz
 	mkdir -p $HOME/dotnet && tar zxf dotnet-sdk.tar.gz -C $HOME/dotnet
 	export DOTNET_ROOT=$HOME/dotnet
 	export PATH=$PATH:$HOME/dotnet
