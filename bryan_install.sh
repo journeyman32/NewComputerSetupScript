d700842db38716cf4240b6820fc32035316a964d
Bryan Pritchett <bryan@coderbox.co>
2023-02-25 18:36:56 -0600

    Update bryan_install.sh

--- bryan_install.sh
+++ bryan_install.sh
@@ -13,15 +13,41 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 	sudo apt install nodejs
 	node -v
 # Install ZSH
-# pacman -S zsh 
-# sudo apt install zsh 
+# 	Arch 	pacman -S zsh 
+# 	Deb 	sudo apt install zsh 
 
 # Install Oh My Zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # Install VM Plugins if runnin as VM
 
 # Utilities
-# 	1Password
+# Deb	1Password
+	curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+	sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+ 	curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+ 	sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+ 	curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+# Arch	1Password
+# Arch	curl -sS https://downloads.1password.com/linux/keys/1password.asc | gpg --import
+# Arch	git clone https://aur.archlinux.org/1password.git
+# Arch	cd 1password
+# Arch	makepkg -si
+#	1Password CLI
+	curl -sS https://downloads.1password.com/linux/keys/1password.asc | \  sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |  sudo tee /etc/apt/sources.list.d/1password.list
+	sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/ 
+	curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+ 	sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+	sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+	curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+ 	sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+ 	sudo apt update && sudo apt install 1password-cli
+# Arch	ARCH="amd64" && \
+# Arch	wget "https://cache.agilebits.com/dist/1P/op2/pkg/v2.14.0/op_linux_${ARCH}_v2.14.0.zip" -O op.zip && \
+# Arch  unzip -d op op.zip && \
+# Arch  sudo mv op/op /usr/local/bin && \
+# Arch  rm -r op.zip op
 
 # Applications
 # 	Jetbrains
